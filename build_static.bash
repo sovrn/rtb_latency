9f582d884e6d2a0ea556d90ce1ccb91697e90905
Jeremy McCoy <jmccoy@sovrn.com>
2018-05-08 13:48:27 -0600

    updated submodules and build scripts

--- build_static.bash
+++ build_static.bash
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+# Ensure that the environment has all the submodules and symlinks in place
+source setup_environment.bash
+
+# Create temporary zip file that conatins rtb_latency and all its dependencies
 zip -9\
   --exclude \*.pyc \*.rst \*.txt \*.bat Makefile\
   --recurse-paths\
   tmp.zip\
-  certifi\
-  chardet\
-  dns\
-  idna\
-  ipwhois\
-  pymysql\
-  requests\
-  urllib3\
-  __main__.py\
-  ipaddr.py\
-  ping.py\
+    certifi\
+    chardet\
+    dns\
+    idna\
+    ipwhois\
+    pymysql\
+    requests\
+    urllib3\
+    __main__.py\
+    ipaddr.py\
+    ping.py\
 
+# Prepend shebang to zip file so Python will execute it
 cat - tmp.zip <<< '#!/usr/bin/env python' > rtb_latency.pyz
+# Set Permissions
 chmod -v a+rx rtb_latency.pyz
+# Remove temporary file
 rm -v tmp.zip
---
+++ setup_environment.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pull in submodules
+git submodule init
+git submodule update --recursive
+
+# Link submodules so that the main script knows how to find the packages
+ln -sfv rtb_latency.py __main__.py
+ln -sfv submodules/python-certifi/certifi certifi
+ln -sfv submodules/chardet/chardet chardet
+ln -sfv submodules/dnspython/dns dns
+ln -sfv submodules/idna/idna idna
+ln -sfv submodules/ipaddr-py/ipaddr.py ipaddr.py
+ln -sfv submodules/ipwhois/ipwhois ipwhois
+ln -sfv submodules/python-ping/ping.py ping.py
+ln -sfv submodules/PyMySQL/pymysql pymysql
+ln -sfv submodules/requests/requests requests
+ln -sfv submodules/urllib3/urllib3 urllib3
